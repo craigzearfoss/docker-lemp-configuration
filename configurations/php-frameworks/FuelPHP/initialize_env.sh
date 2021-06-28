740a423443e3b210bc356b6da373b3c369faba01
Craig Zearfoss <craigzearfoss@yahoo.com>
2021-06-28 11:59:44 -0500

    updates to FuelPHP initialize_env.sh file

--- configurations/php-frameworks/FuelPHP/initialize_env.sh
+++ configurations/php-frameworks/FuelPHP/initialize_env.sh
@@ -4,7 +4,9 @@ echo  "Initializing FuelPHP configuration ..."
 cd /var/www/site
 
 # Set variables
-db_config_file="/var/www/site/fuel/app/config/db.php"
+
+config_file="/var/www/site/fuel/app/config/config.php"
+db_dev_config_file="/var/www/site/fuel/app/config/development/db.php"
 db_admin_port="{{db_admin_port}}"
 db_exposed_port="{{db_exposed_port}}"
 db_name="{{db_name}}"
@@ -17,15 +19,14 @@ port="{{port}}"
 project_name="{{project_name}}"
 service_db="{{service_db}}"
 
-if [[ -f "${db_config_file}" ]]; then
+# Make modifications to config/config.php file
+echo "Modifying config/config.php ..."
+sed -i "s/.*'base_url'.*/    'base_url' => 'http:\/\/localhost:${port}\/',/g" "${config_file}"
 
-  # Update database configuration
-  read -r -d '' dbconn_array << EOF
-return array(
-  'development' => array(),
-EOF
+# Make modifications to config/development/db.php file
+echo "Modifying config/development/db.php ..."
+sed -i "s/.*'dsn'.*/			'dsn'      => 'mysql:host=db-${service_db};port=${db_port};dbname=${db_name,,}',/g" "${db_dev_config_file}"
+sed -i "s/.*'username'.*/			'username' => '${db_username}',/g" "${db_dev_config_file}"
+sed -i "s/.*'password'.*/			'password' => '${db_password}',/g" "${db_dev_config_file}"
 
-  sed -i "s/return array.*/${dbconn_array}/g" "${db_config_file}"
-
-  # @TODO: Need to add updates for email configuration
-fi
+# @TODO: Need to add updates for email configuration
